0c117ed7021b90f20cb76e934c7c7d3fe7336e3d
lorreina <lorreina@sina.com.cn>
2016-09-01 01:10:05 -0400

    update Sep 01 2016

---
+++ LeetCode/src/Bulb_Switcher.java
@@ -0,0 +1,11 @@
+/**
+ * LeetCode
+ * 319. Bulb Switcher
+ * @author lorreina
+ *
+ */
+public class Bulb_Switcher {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
